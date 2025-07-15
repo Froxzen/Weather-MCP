fe887216e2ef95d708c3aba038a1877d660084d3
Froxzen <harvardchong1@gmail.com>
2025-07-15 14:11:06 +1000

    Added comments

--- weather.py
+++ weather.py
@@ -5,6 +5,11 @@ from mcp.server.fastmcp import FastMCP
 # Initialise FastMCP server
 mcp = FastMCP("weather")
 
-# Constants
+# _Constants_
+# Base URL for the National Weather Service (NWS) API 
 NSW_API_BASE = "https://api.weather.gov"
+# A string that identifies your application when making HTTP requests
 USER_AGENT = "weather-app/1.0"
+
+# Helper Functions
+async def make_nws
